f1ecb26f2c9b07316a9e59112857748f6c1878dc
khansrfrz <khansarfaraz205@gmail.com>
2023-06-05 13:56:39 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data.json";
+import Jobs from "./components/Jobs";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
+  const [filterKeywords, setfilterKeywords] = useState([]);
+
+  // const setSearchKeyword = (data) => {
+  //   setfilterKeywords(data);
+  // };
+
+  const addFilterKeywords = (data) => {
+    if (!filterKeywords.includes(data)) {
+      setfilterKeywords([...filterKeywords, data]);
+    }
+  };
+
+  const deleteKeyword = (data) => {
+    const newKeywords = filterKeywords.filter((key) => key !== data);
+    setfilterKeywords(newKeywords);
+  };
+
+  const clearAll = () => {
+    setfilterKeywords([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header"></div>
+
+      {/* <Search setSearchKeyword={setSearchKeyword} /> */}
+
+      {filterKeywords.length > 0 && (
+        <Header
+          keywords={filterKeywords}
+          removeKeywords={deleteKeyword}
+          clearAll={clearAll}
+        />
+      )}
+
+      <Jobs
+        keywords={filterKeywords}
+        data={data}
+        setKeywords={addFilterKeywords}
+      />
     </div>
   );
 }
